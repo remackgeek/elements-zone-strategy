031bd9c1bc674d77c071c095c50c236ec5ac8a1a
nikni <nicola.tommasi.91@gmail.com>
2018-08-19 22:17:21 +0200

    Adding CUSTOM_ELEMENTS_SCHEMA and removin placeholders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { NgModule, Injector, ApplicationRef } from '@angular/core';
+import { NgModule, Injector, ApplicationRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { ElementZoneStrategyFactory } from 'elements-zone-strategy';
 import { createCustomElement } from '@angular/elements';
 import { HelloComponent } from './hello/hello.component';
 import { AppComponent } from './app.component';
-import { ElementAPlaceholderComponent } from './my-custom-element-placeholder/element-a-placeholder.component';
-import { ElementBPlaceholderComponent } from './my-custom-element-placeholder/element-b-placeholder.component';
 
 @NgModule({
   declarations: [
     HelloComponent,
     AppComponent,
-    ElementAPlaceholderComponent,
-    ElementBPlaceholderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  entryComponents: [HelloComponent, AppComponent]
+  entryComponents: [HelloComponent, AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {
   constructor(private injector: Injector) { }
--- src/app/my-custom-element-placeholder/element-b-placeholder.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'my-custom-element-b',
-    template: '<div></div>'
-})
-export class ElementBPlaceholderComponent { }
